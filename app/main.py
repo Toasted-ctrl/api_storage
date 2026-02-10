fe6a356363b5f965ca52a95f930df3a024eb206e
Toasted-ctrl <themickbb@gmail.com>
2026-02-10 19:36:07 +0100

    General updates. Moved pydantic models to "models.py" and authorization logic to "auth.py".

--- app/main.py
+++ app/main.py
@@ -1,37 +1,20 @@
-import os
-import json
-
+from app import models
+from app import auth
 from dotenv import load_dotenv
-from pydantic import BaseModel
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends
 from fastapi.security import APIKeyHeader
 
 load_dotenv()
 
 app = FastAPI()
 api_key_header = APIKeyHeader(name="API_KEY")
-test_key = os.getenv("test_key")
-
-class SuccessfulReturn(BaseModel):
-    message: str
-
-def is_allowed_post(api_key: str, access_type: str):
-    if api_key != test_key: #TODO: Build function to verify api key
-        raise HTTPException(401)
     
-@app.get("/")
+@app.get("/", response_model=models.ReturnSuccessSimple)
 def read_root():
     return {"message": "Hello World"}
 
-class SingleEntry(BaseModel):
-    database: str
-    table: str
-    url: str
-    params: dict | None
-    data: dict
-
-@app.post("/post_entry", response_model=SuccessfulReturn)
-def post_single_entry(entry: SingleEntry, api_key=Depends(api_key_header)):
-    is_allowed_post(api_key=api_key, access_type='r')
+@app.post("/post_single", response_model=models.ReturnSuccessSimple)
+def post_single_entry(entry: models.PostSingleEntry, api_key=Depends(api_key_header)):
+    auth.is_allowed_post(api_key=api_key, access_type='r')
     print(entry.data)
     return {"message": "success"}
