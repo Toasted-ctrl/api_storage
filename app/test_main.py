2c7a013c45f7aea5d5a0d1334db2d25ef860aae0
Toasted-ctrl <themickbb@gmail.com>
2026-02-13 18:35:31 +0100

    Added test for database server error.

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,8 @@
-from fastapi.testclient import TestClient
+import pytest
+
 from app.main import app
+from fastapi.testclient import TestClient
+from unittest.mock import patch, MagicMock
 
 client = TestClient(app=app)
 
@@ -19,11 +22,10 @@ class TestPostSingleEntry:
         assert response.status_code == 401
         assert response.json() == {"detail": "Not authenticated"}
 
-    def test_incorrect_api_key(self):
+    def test_incorrect_api_key(self): # TODO: Rewrite test with patch for database connection 
         response = client.post(url='/post_single',
-                               headers={"API_KEY":"122"},
-                               json={"database": "test_database",
-                                     "table": "test_table",
+                               headers={"API_KEY": "122"},
+                               json={"table": "test_table",
                                      "url": "test_url",
                                      "data": {
                                          "data_1": "data_1_ok"
@@ -31,6 +33,18 @@ class TestPostSingleEntry:
         
         assert response.status_code == 401
         assert response.json() == {"detail": "Unauthorized"}
+
+    def test_database_server_error(self):
+        response = client.post(url='/post_single',
+                               headers={"API_KEY": "122"},
+                               json={"table": "test_table",
+                                     "url": "test_url",
+                                     "data": {
+                                         "data_1": "data_1_ok"
+                                     }})
+        
+        assert response.status_code == 500
+        assert response.json() == {"detail": "Internal Server Error"}
 
     def test_missing_data(self):
         response = client.post(url='/post_single',
