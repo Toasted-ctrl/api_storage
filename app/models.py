4ccbd6a045fd77e5be266a08c38b96ed5b2242d6
Toasted-ctrl <themickbb@gmail.com>
2026-02-14 14:42:20 +0100

    Minor changes to naming, addition of ReturnPostSingle.

--- app/models.py
+++ app/models.py
@@ -1,10 +1,15 @@
 from pydantic import BaseModel
 
-class PostSingleEntry(BaseModel):
+class InputSingleEntry(BaseModel):
     table: str
     url: str
     params: dict | None = None
     data: dict
 
-class ReturnSuccessSimple(BaseModel):
+class ReturnSimple(BaseModel):
     message: str
+
+class ReturnPostSingle(BaseModel):
+    message: str
+    user_id: str
+    data: dict
