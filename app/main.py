069e206bb0d7b2002dfa2907b9fe54257d3804ee
Toasted-ctrl <themickbb@gmail.com>
2026-02-14 14:41:52 +0100

    Reworked auth.verify_api_key and included tests for "/post_single" function.

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import os
+
 from app import models
 from app import auth
 from dotenv import load_dotenv
@@ -8,12 +10,16 @@ load_dotenv()
 
 app = FastAPI()
 api_key_header = APIKeyHeader(name="API_KEY")
+
+db_database_ingest = os.getenv("db_database_ingest")
     
-@app.get("/", response_model=models.ReturnSuccessSimple)
+@app.get("/", response_model=models.ReturnSimple)
 def read_root():
     return {"message": "Hello World"}
 
-@app.post("/post_single", response_model=models.ReturnSuccessSimple)
-def post_single(entry: models.PostSingleEntry, api_key=Depends(api_key_header)):
-    user_id = auth.verify_api_key(api_key=api_key)
-    return {"message": "success"}
+@app.post("/post_single", response_model=models.ReturnPostSingle)
+def post_single(entry: models.InputSingleEntry, api_key: str = Depends(api_key_header)):
+    user_id = auth.verify_api_key(api_key=api_key, database=db_database_ingest, access_type='Write-only')
+    return {"message": "Data added successfully", "user_id": user_id, "data": entry.data}
+
+# TODO: Build new function where auth is handled as dependency of main function call.
--- app/test_main.py
+++ app/test_main.py
@@ -1,14 +1,12 @@
-import pytest
-
 from app.main import app
+from fastapi import HTTPException
 from fastapi.testclient import TestClient
-from unittest.mock import patch, MagicMock
 
 client = TestClient(app=app)
 
 class TestReadRoot:
 
-    def test_read_root(self):
+    def test_success(self):
         response = client.get(url='/')
         
         assert response.status_code == 200
@@ -16,35 +14,72 @@ class TestReadRoot:
 
 class TestPostSingleEntry:
 
+    def test_success(self, monkeypatch):
+
+        # Overriding auth.verify_api_key to test successful db call
+        def override_verify_api_key(*args, **kwargs):
+            return 'test_user_123'
+        
+        monkeypatch.setattr("app.main.auth.verify_api_key", override_verify_api_key)
+
+        response = client.post(url='/post_single',
+                               headers={"API_KEY": "122"},
+                               json={"table": "test_table",
+                                     "url": "test_url",
+                                     "data": {
+                                         "test_data_1": "test_data_1_OK",
+                                         "test_data_2": "test_data_2_OK"}})
+        
+        assert response.status_code == 200
+        assert response.json() == {"message": "Data added successfully",
+                                   "user_id": "test_user_123",
+                                   "data": {
+                                       "test_data_1": "test_data_1_OK",
+                                       "test_data_2": "test_data_2_OK"}}
+        
+    def test_auth_db_error(self, monkeypatch):
+
+        # Overriding auth.verify_api_key to throw an error on db connection
+        def raise_postgres_server_error(*args, **kwargs):
+            raise HTTPException(status_code=500)
+        
+        monkeypatch.setattr("app.main.auth.verify_api_key", raise_postgres_server_error)
+
+        response = client.post(url='/post_single',
+                               headers={"API_KEY": "122"},
+                               json={"table": "test_table",
+                                     "url": "test_url",
+                                     "data": {
+                                         "test_data_1": "test_data_1_OK",
+                                         "test_data_2": "test_data_2_OK"}})
+        
+        assert response.status_code == 500
+        assert response.json() == {"detail": "Internal Server Error"}
+
+    def test_invalid_api_key(self, monkeypatch):
+
+        # Overriding auth.verify_api_key to throw an invalid api key error
+        def raise_invalid_api_key(*args, **kwargs):
+            raise HTTPException(status_code=401)
+        
+        monkeypatch.setattr("app.main.auth.verify_api_key", raise_invalid_api_key)
+
+        response = client.post(url='/post_single',
+                               headers={"API_KEY": "invalid_api_key"},
+                               json={"table": "test_table",
+                                     "url": "test_url",
+                                     "data": {
+                                         "test_data_1": "test_data_1_OK",
+                                         "test_data_2": "test_data_2_OK"}})
+        
+        assert response.status_code == 401
+        assert response.json() == {"detail": "Unauthorized"}
+
     def test_missing_api_key(self):
         response = client.post(url='/post_single')
         
         assert response.status_code == 401
         assert response.json() == {"detail": "Not authenticated"}
-
-    def test_incorrect_api_key(self): # TODO: Rewrite test with patch for database connection 
-        response = client.post(url='/post_single',
-                               headers={"API_KEY": "122"},
-                               json={"table": "test_table",
-                                     "url": "test_url",
-                                     "data": {
-                                         "data_1": "data_1_ok"
-                                     }})
-        
-        assert response.status_code == 401
-        assert response.json() == {"detail": "Unauthorized"}
-
-    def test_database_server_error(self):
-        response = client.post(url='/post_single',
-                               headers={"API_KEY": "122"},
-                               json={"table": "test_table",
-                                     "url": "test_url",
-                                     "data": {
-                                         "data_1": "data_1_ok"
-                                     }})
-        
-        assert response.status_code == 500
-        assert response.json() == {"detail": "Internal Server Error"}
 
     def test_missing_data(self):
         response = client.post(url='/post_single',
