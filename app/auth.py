4e814d8b0b8c84659dc4d6107fd25da462ae45b2
Toasted-ctrl <themickbb@gmail.com>
2026-02-14 14:40:12 +0100

    Minor updates to verify_api_key.

--- app/auth.py
+++ app/auth.py
@@ -7,22 +7,19 @@ from fastapi import HTTPException
 load_dotenv()
 
 db_hostname = os.getenv("db_hostname")
-db_database_api_key = os.getenv("db_database_api_keys")
-db_database_ingest = os.getenv("db_database_ingest")
+db_database = os.getenv("db_database_api_keys")
 db_password = os.getenv("db_password")
 db_table = os.getenv("db_table_api_key_users")
 db_user = os.getenv("db_user")
 db_port = os.getenv("db_port")
 
-test_key = os.getenv("test_key")
-
-def verify_api_key(api_key: str) -> str:
+def verify_api_key(api_key: str, database: str, access_type: str) -> str:
 
     try:
 
         conn = psycopg2.connect(
             host=db_hostname,
-            database=db_database_api_key,
+            database=db_database,
             user=db_user,
             password=db_password,
             port=db_port
@@ -34,18 +31,25 @@ def verify_api_key(api_key: str) -> str:
         # Execute query
         cur.execute(f"""SELECT id FROM {db_table}
                     WHERE api_key = '{api_key}'
-                    AND database = '{db_database_ingest}'
-                    AND write_access = 'Yes'""")
+                    AND database = '{database}'
+                    AND access_type = '{access_type}'""")
 
         # Fetch one result
         user_id = cur.fetchone()
 
-        conn.close()
-
         if user_id == None:
-            raise HTTPException(401)
+            raise HTTPException(status_code=401)
         
         return user_id
+    
+    except HTTPException as httpe:
+        raise HTTPException(status_code=httpe.status_code, detail=httpe.detail)
 
     except psycopg2.Error:
         raise HTTPException(500)
+    
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()
