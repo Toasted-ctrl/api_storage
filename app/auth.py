8c1cf4a31667f95b07c44559b7f62429b1af7638
Toasted-ctrl <themickbb@gmail.com>
2026-02-23 19:27:31 +0100

    Minor changes to verify_resource_access(), to return a bool.

--- app/auth.py
+++ app/auth.py
@@ -2,8 +2,7 @@ from app.database import ApiKeys, Users
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
-def verify_api_key(database: Session,
-                   api_key: str) -> dict:
+def verify_api_key(database: Session, api_key: str) -> dict:
     
     user_id = database.query(ApiKeys).filter(ApiKeys.api_key == api_key).first()
     if user_id == None:
@@ -14,18 +13,18 @@ def verify_resource_access(database: Session,
                   user_id: int,
                   can_read: bool | None = None,
                   can_write: bool | None = None,
-                  is_admin: bool | None = None) -> None:
+                  is_admin: bool | None = None) -> bool:
     
     if can_read != None and can_write == None and is_admin == None:
-        query_result = database.query(Users).filter(Users.id == user_id, Users.can_read == can_read).first()
+        query_result = database.query(Users).filter(Users.user_id == user_id, Users.can_read == can_read).first()
 
     elif can_read == None and can_write != None and is_admin == None:
-        query_result = database.query(Users).filter(Users.id == user_id, Users.can_write == can_write).first()
+        query_result = database.query(Users).filter(Users.user_id == user_id, Users.can_write == can_write).first()
         
     elif is_admin != None:
-        query_result = database.query(Users).filter(Users.id == user_id, Users.is_admin == is_admin).first()
+        query_result = database.query(Users).filter(Users.user_id == user_id, Users.is_admin == is_admin).first()
 
     if query_result == None:
         raise HTTPException(403)
         
-    return None
+    return True
