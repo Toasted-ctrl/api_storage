7b692a8f3264d4ce18d076148e68c4ad5225a630
Toasted-ctrl <themickbb@gmail.com>
2026-02-26 07:08:33 +0100

    Minor updates to remove hardcoded dependencies db_type and db_conn.

--- app/database.py
+++ app/database.py
@@ -10,13 +10,16 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
+# Loading all database requirements.
 hostname = os.getenv("hostname")
 database = os.getenv("database")
 password = os.getenv("password")
 username = os.getenv("user")
 port = os.getenv("port")
+db_type = os.getenv("db_type")
+db_conn = os.getenv("db_connection")
 
-database_url = f"postgresql+psycopg2://{username}:{password}@{hostname}:{port}/{database}"
+database_url = f"{db_type}+{db_conn}://{username}:{password}@{hostname}:{port}/{database}"
 
 engine = create_engine(url=database_url)
 db_session = sessionmaker(autocommit = False, autoflush = False, bind = engine)
@@ -38,7 +41,9 @@ class ApiKeys(base):
 class Users(base):
     __tablename__ = 'users'
 
-    user_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    # Set unique=True to ensure email addresses can only be added once.
+    user_id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
     first_name = Column(String(100), nullable=False)
     last_name = Column(String(100), nullable=False)
     is_admin = Column(Boolean, nullable=False)
@@ -52,6 +57,7 @@ class Ingest(base):
     url_primary = Column(String(100), nullable=False)
     url_extension = Column(String(100), nullable=True)
     params = Column(JSON, nullable=True)
+    status_code = Column(Integer, nullable=False)
     data = Column(JSON, nullable=False)
 
     # Will add datetime of when the entry was added on the server side.
