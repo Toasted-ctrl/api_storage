65f0f9036bbe21b58da33baeb76a2ad055a22914
Toasted-ctrl <themickbb@gmail.com>
2026-03-13 21:34:56 +0100

    Removed.

--- app/api/v1/status.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from app.models2 import status
-
-router = APIRouter()
-
-@router.get("/status", response_model=status.ReturnStatus, tags=["Status"])
-def get_health():
-    return {"status": "OK"}
