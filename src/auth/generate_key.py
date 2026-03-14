12d88438c51039a6142d3e17765b067eaf88e8fb
Toasted-ctrl <themickbb@gmail.com>
2026-03-14 13:24:50 +0100

    Updated docs.

--- src/auth/generate_key.py
+++ src/auth/generate_key.py
@@ -3,7 +3,10 @@ import secrets
 from src.auth.hashing import hash_sha256
 
 def generate_keys() -> tuple[str, str]:
-    """Returns a tuple, where [0] is the key, and [1] the hashed key"""
+    
+    """
+    Returns a tuple, where [0] is the key, and [1] the hashed key
+    """
 
     key = secrets.token_urlsafe(nbytes=32)
     hashed_key = hash_sha256(input=key)
