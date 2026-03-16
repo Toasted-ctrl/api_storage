855ec7eed7cd941deaaf06229e77fc6d1745281e
Toasted-ctrl <themickbb@gmail.com>
2026-03-16 07:04:45 +0100

    Implemented get_user().

--- src/api/v1/user.py
+++ src/api/v1/user.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from auth.permissions import require_permission
 from database.session import get_db
-from models.user import PayloadNewUser, ReturnNewUser, ReturnUsers
+from models.user import PayloadNewUser, ReturnNewUser, ReturnUsers, ReturnUser
 from services.user_service import UserService
 
 # NOTE: Adding dependency in the route since we don't need the user object after
@@ -14,20 +14,20 @@ def get_user_service(session=Depends(get_db)) -> UserService:
     return UserService(session=session)
 
 @router.get("/users", tags=tags, response_model=ReturnUsers)
-def get_users(user_service: UserService = Depends(get_user_service)):
+def get_users(user_service: UserService=Depends(get_user_service)):
     users = user_service.get_users()
     if not users:
-        raise HTTPException(status_code=404, detail="No users in 'users' table")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No users in 'users' table")
     return {
         "detail": "Success",
         "users": users
     }
 
 @router.post("/users", tags=tags, response_model=ReturnNewUser)
-def post_user(payload: PayloadNewUser, user_service: UserService = Depends(get_user_service)):
+def post_user(payload: PayloadNewUser, user_service: UserService=Depends(get_user_service)):
     new_user = user_service.post_user(data=payload.model_dump())
     if not new_user:
-        raise HTTPException(status_code=500, detail="Unable to add user")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Unable to add user")
     return {
         "detail": "Success",
         "new_user": {
@@ -36,5 +36,15 @@ def post_user(payload: PayloadNewUser, user_service: UserService = Depends(get_u
         }
     }
 
+# TODO: Implement tests for the below function.
+@router.get("/users/{user_id}", tags=tags, response_model=ReturnUser)
+def get_user(user_id: int, user_service: UserService=Depends(get_user_service)):
+    user = user_service.get_user(id=user_id)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return {
+        "detail": "Success",
+        "user": user
+    }
+
 # TODO: Add function to update user.
-# TODO: Add function to fetch one user's information.
