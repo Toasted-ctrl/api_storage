6e54e0f6cb80ee85c03441d9614c7ab0de14f3bf
Toasted-ctrl <themickbb@gmail.com>
2026-03-16 06:50:45 +0100

    Added override_user_service fixture.

--- tests/fixtures/services.py
+++ tests/fixtures/services.py
@@ -1,5 +1,6 @@
 import pytest
 from api.v1.data import get_data_service
+from api.v1.user import get_user_service
 
 class MockDataService:
     def post_data(self, data):
@@ -28,5 +29,37 @@ def mock_data_service():
 @pytest.fixture
 def override_data_service(app, mock_data_service):
     app.dependency_overrides[get_data_service] = lambda: mock_data_service
+    yield
+    app.dependency_overrides.clear()
+
+class MockUserService:
+    def get_users(self):
+        return [
+            {"email": "TEST_MAIL_123",
+             "user_id": 1}
+        ]
+    
+    def post_user(self, data):
+        return (
+            {
+                "first_name": "TEST_FIRST_NAME",
+                "last_name": "TEST_LAST_NAME",
+                "email": "TEST_MAIL",
+                "is_admin": True,
+                "can_write": True,
+                "can_read": True,
+                "id": 2,
+                "is_active": True
+            },
+            "TEST_KEY_123"
+        )
+
+@pytest.fixture
+def mock_user_service():
+    return MockUserService()
+
+@pytest.fixture
+def override_user_service(app, mock_user_service):
+    app.dependency_overrides[get_user_service] = lambda: mock_user_service
     yield
     app.dependency_overrides.clear()
